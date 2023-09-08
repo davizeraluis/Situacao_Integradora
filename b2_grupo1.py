b3c588266abc7486fdd8dd42ef8484983e4f2f7c
davizeraluis <66173353+davizeraluis@users.noreply.github.com>
2023-09-08 20:58:02 -0300

    Add files via upload

--- b2_grupo1.py
+++ b2_grupo1.py
@@ -10,6 +10,19 @@ Turma B2 - Grupo 1
 7804 Maria Júlia
 7832 Vitória Gomes
 '''
+#Bibliotecas
+import math
+
+#Funçoes
+#Função que converte graus em radianos
+def convercao_graus_rad(angulo_graus):
+    angulo_rad=math.radians(angulo_graus)
+    return angulo_rad
+#Função que calcula o limite por definição
+#Método de Newton
+#def metodo_newton(palpite,iteracoes):
+    
+
 #Entrada de Dados
 #Entrada dos parâmetros da função
 angulo_lancamento=float(input("Digite o ângulo de lançamento (em graus): "))
@@ -17,11 +30,18 @@ velocidade_inicial=float(input("Digite a velocidade incial: "))
 gravidade=float(input("Digite o valor da gravidade: "))
 altura_inicial=float(input("Digite a altura inicial do lançamento: "))
 coeficiente_arrasto=float(input("Digite o coeficiente de arrasto: "))
-
 #Entrada do parâmetros do método de newton
 valor_inicial=float(input("Digite um valor inicial aproximado para o alcance: "))
 numero_iteracoes=float(input("Digite o número de iterações desejada: "))
-resultado=float(0)
+x=float(0)
+#Resultado final
+resultado_final=float(0)
 
-#Constantes
-EULER=float(2,718281)
+#Processamento
+angulo_lancamento=convercao_graus_rad(angulo_lancamento)
+#Chamar a função metodo de newton
+
+#Fórmula do lançamento oblíquo
+for i in range(0,numero_iteracoes):
+    resultado_final=float(altura_inicial+(math.tan(angulo_lancamento)+(gravidade/(coeficiente_arrasto*velocidade_inicial*math.cos(angulo_lancamento))))*x+(gravidade/coeficiente_arrasto**2)*math.log(1-coeficiente_arrasto/(velocidade_inicial*math.cos(angulo_lancamento))*x))
+    print(resultado_final)
